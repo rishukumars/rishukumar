ed5ea6d018cc9374deb6378af5951913c43faf47
Rishu Kumar <rishukrajput453@gmail.com>
2024-06-01 00:11:34 +0530

    changes

--- src/components/MainHead.jsx
+++ src/components/MainHead.jsx
@@ -1,33 +1,47 @@
-import { useState } from "react"
+import { useState } from "react";
 
+function MainHead({ handleOnClick }) {
+  let [todoName, setTodoName] = useState("");
+  let [todoDate, setTodoDate] = useState("");
 
+  let handleName = (event) => {
+    setTodoName(event.target.value);
+  };
 
+  let handleDate = (event) => {
+    setTodoDate(event.target.value);
+  };
 
-function MainHead({handleOnClick}){
+  let handleAddClick = (event) => {
+    console.log(event)
+    event.preventDefault()
+    handleOnClick(todoName, todoDate);
+    setTodoName("");
+    setTodoDate("");
+  };
 
-  let [todoName, setTodoName] = useState("")
-  let [todoDate, setTodoDate] = useState("")
-
-  let handleName = (event) =>{
-     setTodoName(event.target.value)
-  }
-
-  let handleDate = (event) =>{
-    setTodoDate(event.target.value)
-  }
-
-  let handleAddClick = () => {
-    handleOnClick(todoName,todoDate)
-    setTodoName("")
-    setTodoDate("")
-  }
-
-  return <>
-  <input type='text' className='x' placeholder='type you want' value={todoName}  onChange={(event) => handleName(event)} />
-  <input type='date' className='x' value={todoDate} onChange={(event) => handleDate(event)} />
-  <button className='add' onClick={handleAddClick}>Add</button>
-  </>
+  return (
+    <form action="/" onSubmit={handleAddClick}>
+      <input
+        type="text"
+        className="x"
+        placeholder="type you want"
+        value={todoName}
+        onChange={(event) => handleName(event)}
+      />
+      <input
+        type="date"
+        className="x"
+        value={todoDate}
+        onChange={(event) => handleDate(event)}
+      />
+      <button
+       className="add"
+        type="submit">
+        Add
+      </button>
+    </form>
+  );
 }
 
-
-export default MainHead
+export default MainHead;
